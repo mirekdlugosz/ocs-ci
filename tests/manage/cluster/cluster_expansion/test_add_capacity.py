05505c3f031cebf1bc912b2daa133e7d4cf4c2bd
Shreekar <sshreeka@redhat.com>
2019-09-25 13:30:26 +0530

    tests/manage/cluster/cluster_expansion/test_add_capacity.py  - Updated var osd_count to use new function pod.get_pod_count()
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/cluster/cluster_expansion/test_add_capacity.py
+++ tests/manage/cluster/cluster_expansion/test_add_capacity.py
@@ -1,9 +1,9 @@
 import logging
 
-from tests import helpers
 from ocs_ci.framework.testlib import tier1, ManageTest
 from ocs_ci.ocs import machine as machine_utils
 from ocs_ci.ocs import constants, ocp
+from ocs_ci.ocs.resources import pod
 
 
 logger = logging.getLogger(__name__)
@@ -12,12 +12,11 @@ logger = logging.getLogger(__name__)
 @tier1
 class TestAddCapacity(ManageTest):
     def test_add_capacity(self):
-        osd_count = helpers.get_osd_count()
+        osd_count = pod.get_pod_count(label=constants.OSD_APP_LABEL)
         storage_cluster = machine_utils.get_storage_cluster()
-
-        machine_utils.add_capacity(storagecluster_name=storage_cluster,count=osd_count+3)
-        pod = ocp.OCP(kind=constants.POD)
-        assert pod.wait_for_resource(
+        machine_utils.add_capacity(storagecluster_name=storage_cluster, count=osd_count + 3)
+        pod_obj = ocp.OCP(kind=constants.POD)
+        assert pod_obj.wait_for_resource(
             condition=constants.STATUS_RUNNING, selector=constants.OSD_APP_LABEL,
-            resource_count=osd_count+3, timeout=600
+            resource_count=osd_count + 3, timeout=600
         )
